b69cff453ac906cbf51031c0a8235d0cf2b741a6
unknown <brendenkcheng@gmail.com>
2023-06-13 19:01:20 -0600

    cleaned up file structure and script

---
+++ TODO.md
@@ -0,0 +1,14 @@
+# Equinox Bot TODO
+
+## Project Outline
+    - [ ] Outline / plan features
+    - [ ] Intro database into project
+    - [ ] Find hosting service
+    - [ ] Set up docker?
+
+
+## Fixes / Bugs
+    - [ ] 'scripts\equinox.py' Need database for tasks
+    - [ ] 'scripts\equinox.py' Revamp calendar command
+    - [ ] 'scripts\equinox.py' help command is broken
+    - [ ] 'scripts\equinox.py' remove on_message event that checks for "@Equinox"
--- scripts/equinox.py
+++ scripts/equinox.py
@@ -1,21 +1,26 @@
+import dotenv
+import os
 import discord 
 from discord.ext import commands
 from discord import app_commands
 import string
 
+dotenv.load_dotenv('../.env')
+
 intent = discord.Intents.all()
-command_prefixes=['/', '@']
-bot = discord.Client(intents=intent, command_prefixes=command_prefixes) #how to activate your bot.
+command_prefixes=['/']
+bot = discord.Client(intents=intent, command_prefixes=command_prefixes)
 tree = app_commands.CommandTree(bot)
-#example /write  
 
-
+#EVENTS
+#initializing bot
 @bot.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=868279391074021377))
     print("Bot is connected and ready to go.")
     print("Tree has been sync'd")
 
+#dont keep this, at least, don't keep this on an event
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -23,9 +28,13 @@ async def on_message(message):
     if message.content == "@Equinox":
          await message.channel.send("Equinox is a bot developed by Synthix Labs")
 
+#COMMANDS
+#help command
 @tree.command(name="help", description="Help for commands", guild=discord.Object(id=868279391074021377))
 async def help(ctx):
     ctx.send("Welcome to TaskPlanner! There are currently 3 commands available. 'Run' To set a task to become active. 'List' To see your current tasks running. and 'Help' For more information. ")
+
+#task command
 @tree.command(name="task", description="Create, Manage, View tasks", guild=discord.Object(id=868279391074021377))
 async def task(ctx, arg:str, arg2:str):
     if arg == "list":
@@ -34,6 +43,9 @@ async def task(ctx, arg:str, arg2:str):
         lists = []
         tasks = lists.append(arg)
         await ctx.send("You have successfully added" + tasks)
+
+"""
+#calendar command
 @tree.command(name="calender", description="graphical calender view ", guild=discord.Object(id=868279391074021377))
 async def Calender(interaction: discord.Interaction):
     for i in range(0,6):
@@ -41,11 +53,17 @@ async def Calender(interaction: discord.Interaction):
         print(i)
         for i in range(0,6):
             print(i + 1)
+"""
+
+#name command
 @tree.command(name="name", description="description", guild=discord.Object(id=868279391074021377))
 async def slash_command(interaction: discord.Interaction):    
     await interaction.response.send_message("command")
 
+#test command
 @tree.command(name="test", description="descrip")
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
-bot.run("MTA2NzI5NTIwNzE0NzcyMDc0NA.G52L22.RhkeI-feEfL8-DlrMUtiOMdUm7im4bFQUtP2oM")
+
+#run
+bot.run(os.getenv('DISCORD_TOKEN'))
